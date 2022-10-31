ed82c19d34c9bab018796672de112eb8f54fbc59
Hongmin <hongmin@Hongminui-MacBookPro.local>
2022-11-01 02:39:16 +0900

    [feature] information model

--- routes/metadata.js
+++ routes/metadata.js
@@ -5,35 +5,65 @@ const { firestore } = require('../firesbase/firebase-admin')
 const { verifyToken } = require('../middlewares/auth')
 
 router.get('/listInformationModel', verifyToken, async function (req, res, next) {
-  
-})
-
-router.get('/getInformationModel', verifyToken, async function (req, res, next) {
   try {
-    let { imageName, imageTag } = req.query
+    const { classification } = req.query
 
-    if (!imageName) {
+    if (!classification) {
       res.status(400).json({
         statusCode: 400,
-        message: 'Error: Invalid parameter.\nName of source image(module) is required'
+        message: 'Error: Invalid parameter.\nclassification is required.'
       })
       return
     }
 
+    const classSnapshot = await firestore.collection(`info_models/class/${classification}`).get()
+    const classList = []
+
+    classSnapshot.forEach(doc => {
+      classList.push(doc.id)
+    })
+
+    res.status(200).json({
+      list: classList
+    })
+  } catch (err) {
+    console.log(`Error: GET /listInformationModel.\n${err}`)
+    res.status(500).send(err)
+  }
+})
+
+router.get('/getInformationModel', verifyToken, async function (req, res, next) {
+  try {
+    const { imageName, imageTag } = req.query
+
+    if (!imageName) {
+      res.status(400).json({
+        statusCode: 400,
+        message: 'Error: Invalid parameter.\nName of source image(module) is required.'
+      })
+      return
+    }
+
+    const replacedImageName = imageName.replaceAll('#', '/')
     if (!imageTag) imageTag = 'latest'
 
-    const imageRef = firestore.collection(`images/${imageName}/${imageTag}`).doc('metadata')
+    const imageRef = firestore.collection(`images/${replacedImageName}/${imageTag}`).doc('metadata')
     const imageDoc = await imageRef.get()
+
+    const imageMetadataRef = firestore.collection(`images/${replacedImageName}/common`).doc('metadata')
+    const imageMetadataDoc = await imageMetadataRef.get()
 
     if (!imageDoc.exists) {
       res.status(400).json({
         statusCode: 400,
-        message: 'Error: Image source is not exist'
+        message: 'Error: Image source is not exist.'
       })
     } else {
+      const imageMetadataData = imageMetadataDoc.exists ? imageMetadataDoc.data() : {}
       res.status(200).json({
         // message: 'success'
-        informationModel: imageDoc.data()
+        ...imageDoc.data(),
+        ...imageMetadataData
       })
     }
   } catch (err) {
@@ -44,21 +74,39 @@ router.get('/getInformationModel', verifyToken, async function (req, res, next) 
 
 router.post('/setInformationModel', verifyToken, async function (req, res, next) {
   try {
-    let { imageName, imageTag, informationModel, overwrite } = req.body
+    const { imageName, imageTag, informationModel, overwrite, classification, moduleClass } = req.body
+    const bodyData = { ...req.body }
 
-    if (!imageName || !informationModel) {
+    if (!imageName || !informationModel || !classification || !moduleClass) {
       res.status(400).json({
         statusCode: 400,
-        message: 'Error: Invalid parameter.'
+        message: 'Error: Invalid parameter.\nimageName, informationModel, classification, and, moduleClass are required.'
       })
       return
     }
 
+    delete bodyData['imageName']
+    delete bodyData['informationModel']
+    delete bodyData['classification']
+    delete bodyData['moduleClass']
+
+    if (!Array.isArray(classification)) {
+      res.status(400).json({
+        statusCode: 400,
+        message: 'Error: A parameter, classification, has to be array.'
+      })
+      return
+    }
+
+    const replacedImageName = imageName.replaceAll('#', '/')
     if (!imageTag) imageTag = 'latest'
     if (!overwrite) overwrite = false
 
-    const imageRef = firestore.collection(`images/${imageName}/${imageTag}`).doc('metadata')
+    const imageRef = firestore.collection(`images/${replacedImageName}/${imageTag}`).doc('metadata')
     const imageDoc = await imageRef.get()
+
+    const imageMetadataRef = firestore.collection(`images/${replacedImageName}/common`).doc('metadata')
+    const imageMetadataDoc = await imageMetadataRef.get()
 
     if (!imageDoc.exists) {
       res.status(400).json({
@@ -66,20 +114,52 @@ router.post('/setInformationModel', verifyToken, async function (req, res, next)
         message: 'Error: Image source is not exist'
       })
     } else {
-      // if (typeof metadata !== 'object') {
-      //   res.status(400).json({
-      //     statusCode: 400,
-      //     message: 'Error: metadata should be JSON format'
-      //   })
+      const classArray = imageMetadataDoc.exists ? imageMetadataDoc.data().classification : []
 
-      //   return
-      // }
+      const addClassList = []
+      const removeClassList = []
+
+      classification.forEach(className => {
+        if (!classArray.includes(className)) {
+          addClassList.push(className)
+        }        
+      })
+
+      classArray.forEach(className => {
+        if (!classification.includes(className)) {
+          removeClassList.push(className)
+        }    
+      })
+
+      // remove class reference
+      const classRemovePromises = removeClassList.map(async className => {
+        const classRef = firestore.collection(`info_models/class/${className}`).doc(replacedImageName)
+        await classRef.delete()
+      })
+      await Promise.all(classRemovePromises)
+
+      // set information model class reference
+      const classAddPromises = addClassList.map(async className => {
+        const classRef = firestore.collection(`info_models/class/${className}`).doc(replacedImageName)
+        await classRef.set({
+          reference: `images/${replacedImageName}/common`,
+          updatedAt: new Date().toISOString()
+        })
+      })     
+      await Promise.all(classAddPromises)
 
       try {
         await imageRef.set({
           informationModel,
-          updatedAt: new Date().toISOString()
+          updatedAt: new Date().toISOString(),
+          ...bodyData
         }, { merge: !overwrite })
+
+        await imageMetadataRef.set({
+          moduleClass,
+          classification,
+          updatedAt: new Date().toISOString()
+        })
 
         res.status(200).json({
           statusCode: 200
@@ -88,7 +168,7 @@ router.post('/setInformationModel', verifyToken, async function (req, res, next)
       } catch (err) {
         res.status(400).json({
           statusCode: 400,
-          message: 'Error: fail to set information model to firebase'
+          message: 'Error: fail to set information model to firebase.'
         })
       }
     }
